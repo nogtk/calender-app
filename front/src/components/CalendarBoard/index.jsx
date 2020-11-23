1116d2d9ae8eeec528152cd3769ff42adf859c8d
nogtk <taka.naoga@moneyforward.co.jp>
2020-11-23 13:50:34 +0900

    Revive

---
+++ front/src/components/CalendarBoard/index.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+import dayjs from "dayjs";
+import "dayjs/locale/ja";
+
+dayjs.locale("ja");
+
+import { GridList } from "@material-ui/core";
+import * as styles from "./style.css";
+
+const createCalendar = () => {
+    const firstDay = dayjs().startOf("month");
+    const firstDayIndex = firstDay.day();
+    return Array(35).fill(0).map((_, i) => {
+        const diffFromFirstDay = i - firstDayIndex;
+        const day = firstDay.add(diffFromFirstDay, "day")
+
+        return day;
+    });
+};
+
+const calendar = createCalendar();
+
+const CalendarBoard = () => {
+    return(
+        <div className={styles.container}>
+            <GridList className={styles.grid} cols={7} spacing={0} cellHeight="auto">
+                {calendar.map(c => (
+                    <li key={c.toISOString()}>
+                        <div className={styles.element}>{c.format("D")}</div>
+                    </li>
+                ))};
+            </GridList>
+        </div>
+    );
+};
+
+export default CalendarBoard;
